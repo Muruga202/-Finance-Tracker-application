45471f916a397120d94489553d86e834a90f954d
Maxwell Muruge <maxwell.muruga@student.moringaschool.com>
2025-12-23 13:58:03 +0300

    Add xyz dependency

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,16 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          utils: ['date-fns', 'papaparse']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
   }
 });
